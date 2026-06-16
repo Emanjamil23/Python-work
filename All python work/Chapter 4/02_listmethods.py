1a60cea72014143824a3794b0ac37b82a3b85c10
Eman Jamil <emanjamil358@gmail.com>
2026-06-17 02:12:44 +0500

    Add files via upload

---
+++ All python work/Chapter 4/02_listmethods.py
@@ -0,0 +1,16 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Eman")
+print(friends)
+#other methods
+l1=[12,30,10,5,50]
+# l1.sort()
+# print(l1)
+# l1.reverse()
+# print(l1)
+# l1.insert(3 ,33333)#first index then value
+# print(l1)
+# l1.pop(2)
+# print(l1)
+l1.remove(30)
+print(l1)
---
+++ All python work/Chapter 4/03_tuple.py
@@ -0,0 +1,9 @@
+#making tuple tuples is immutable it cannot change
+a=( 1 ,50,30,"EMAN",False,"Maham")
+print(a)
+#tuples methods
+no=a.count("EMAN")
+print(no)
+i=a.index(50)
+print(i)
+print(len(a))
